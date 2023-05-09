2fdbdc8b6ffcce0475149ca26355c8e34f717c0e
Sandeep <500sandeepks@gmail.com>
2023-05-09 23:37:34 +0530

    zero to the end

--- 1day/movingallzerotoend.java
+++ 1day/movingallzerotoend.java
@@ -10,28 +10,63 @@ public class movingallzerotoend {
      * 
      */
 
-     static void temporary(int[]arr,int n)
-     {
-        int[]temp=new int[n];
-        int k=0;
+    //  static void temporary(int[]arr,int n)
+    //  {
+    //     int[]temp=new int[n];
+    //     int k=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(arr[i]!=0)
+    //         {
+    //             temp[k]=(arr[i]);
+    //             k++;
+    //         }
+    //     }
+    //    while(k<n)
+    //    {
+    //     temp[k]=0;
+    //     k++;
+    //    }
+
+    //    System.out.println(Arrays.toString(temp));
+
+
+    //  }
+
+    //optimal solutin is using two pointer approach;
+
+
+
+
+
+
+    
+    static void temporary(int[]arr,int n)
+    {
+        int j=-1;
         for(int i=0;i<n;i++)
         {
-            if(arr[i]!=0)
+            if(arr[i]==0)
             {
-                temp[k]=(arr[i]);
-                k++;
+                j=i;
+                break;
             }
         }
-       while(k<n)
-       {
-        temp[k]=0;
-        k++;
-       }
+        for(int i=j+1;i<n;i++)
+        {
 
-       System.out.println(Arrays.toString(temp));
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
 
+        System.out.println(Arrays.toString(arr));
 
-     }
+    }
 
      public static void main(String[] args) {
         int[] arr={0,0,0,0,12,3,4,5};
